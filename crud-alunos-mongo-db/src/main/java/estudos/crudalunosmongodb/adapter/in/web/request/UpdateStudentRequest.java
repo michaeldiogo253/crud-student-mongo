51a2bcd050c1a93fd8402fa451175d5d809d9728
MichaelDiogo <michael@petize.com.br>
2022-07-14 23:14:54 -0300

    feat: request class creation

---
+++ crud-alunos-mongo-db/src/main/java/estudos/crudalunosmongodb/adapter/in/web/request/UpdateStudentRequest.java
@@ -0,0 +1,21 @@
+package estudos.crudalunosmongodb.adapter.in.web.request;
+
+import lombok.Value;
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Value
+public class UpdateStudentRequest {
+
+    @NotBlank(message = "Id Student cannot be null or empty") String idStudent;
+    @NotEmpty(message = "Name cannot be empty") String name;
+    @CPF(message = "CPF is not valid") @NotEmpty(message = "CPF cannot be empty") String cpf;
+    @Email(message = "Email is not valid") @NotEmpty(message = "Email cannot be empty") String email;
+    @NotNull(message = "BirthDate cannot be null") LocalDate birthDate;
+
+}
