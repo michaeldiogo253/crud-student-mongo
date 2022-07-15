23d892d1e39aaa654ebfbe6f2001da2bf3764cfc
MichaelDiogo <michael@petize.com.br>
2022-07-14 23:14:05 -0300

    refactor! class exception creation

---
+++ crud-alunos-mongo-db/src/main/java/estudos/crudalunosmongodb/util/exception/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package estudos.crudalunosmongodb.util.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+
+        super(message);
+    }
+}
