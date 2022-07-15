902ccb8269bc394674db618b65b1b487240236a2
MichaelDiogo <michael@petize.com.br>
2022-07-14 21:19:33 -0300

    feat: initial commit

---
+++ crud-alunos-mongo-db/src/test/java/estudos/crudalunosmongodb/CrudAlunosMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package estudos.crudalunosmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudAlunosMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
