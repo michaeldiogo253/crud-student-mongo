319847c357e1fca0ff5d4343fbfa3b8470791ec8
MichaelDiogo <michael@petize.com.br>
2022-07-14 21:59:25 -0300

    refactor! changed http method POST -> GET

--- crud-alunos-mongo-db/src/main/java/estudos/crudalunosmongodb/adapter/in/web/ListAllStudentsController.java
+++ crud-alunos-mongo-db/src/main/java/estudos/crudalunosmongodb/adapter/in/web/ListAllStudentsController.java
@@ -5,7 +5,7 @@ import estudos.crudalunosmongodb.application.port.out.FindAllStudentPort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +18,7 @@ public class ListAllStudentsController {
 
     private final FindAllStudentPort findAllStudentPort;
 
-    @PostMapping(path = "/list-all")
+    @GetMapping(path = "/list-all")
     public ResponseEntity<List<StudentResponse>> execute() {
 
         return ResponseEntity.ok()
