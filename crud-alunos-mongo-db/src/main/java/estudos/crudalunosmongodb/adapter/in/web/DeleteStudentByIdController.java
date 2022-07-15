9a193f9dc08a6c37c58e49f2ec0ad7f1cd1d38ff
MichaelDiogo <michael@petize.com.br>
2022-07-14 21:58:24 -0300

    feat: created controller for delete student by id

---
+++ crud-alunos-mongo-db/src/main/java/estudos/crudalunosmongodb/adapter/in/web/DeleteStudentByIdController.java
@@ -0,0 +1,24 @@
+package estudos.crudalunosmongodb.adapter.in.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/student")
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class DeleteStudentByIdController {
+
+
+    @DeleteMapping(path = "/{idStudent}")
+    public ResponseEntity<Void> execute(@PathVariable("idStudent") String idStudent) {
+
+        return ResponseEntity.noContent()
+                             .build();
+    }
+
+}
