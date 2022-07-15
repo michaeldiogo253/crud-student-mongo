00a3ac557f49662d62e8918715fe99714a788be5
MichaelDiogo <michael@petize.com.br>
2022-07-14 21:14:54 -0300

    feat: create controller class

---
+++ crud-alunos-mongo-db/src/main/java/estudos/crudalunosmongodb/adapter/in/web/RegisterStudentController.java
@@ -0,0 +1,30 @@
+package estudos.crudalunosmongodb.adapter.in.web;
+
+import estudos.crudalunosmongodb.adapter.in.web.request.RegisterStudentRequest;
+import estudos.crudalunosmongodb.application.RegisterStudentUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping(path = "/student")
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class RegisterStudentController {
+
+    private final RegisterStudentUseCase useCase;
+
+    @PostMapping(path = "/register")
+    public ResponseEntity<String> execute(@Valid @RequestBody RegisterStudentRequest request) {
+
+        useCase.execute(request.getName(), request.getCpf(), request.getEmail(), request.getBirthDate());
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+}
