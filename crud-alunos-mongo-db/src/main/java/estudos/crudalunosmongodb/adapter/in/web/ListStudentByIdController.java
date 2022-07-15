c9ec0ab45df10db6485825ece7ae29e5466ce941
MichaelDiogo <michael@petize.com.br>
2022-07-14 21:18:14 -0300

    feat: created controller to list student by id

---
+++ crud-alunos-mongo-db/src/main/java/estudos/crudalunosmongodb/adapter/in/web/ListStudentByIdController.java
@@ -0,0 +1,30 @@
+package estudos.crudalunosmongodb.adapter.in.web;
+
+import estudos.crudalunosmongodb.adapter.in.web.response.StudentResponse;
+import estudos.crudalunosmongodb.application.port.out.FindStudentByIdPort;
+import estudos.crudalunosmongodb.domain.Student;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/student")
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class ListStudentByIdController {
+
+    private final FindStudentByIdPort findStudentByIdPort;
+
+    @PostMapping(path = "/{idStudent}")
+    public ResponseEntity<StudentResponse> execute(@PathVariable("idStudent") String idStudent) {
+
+        Student student = findStudentByIdPort.findStudentById(idStudent);
+
+        return ResponseEntity.ok()
+                             .body(StudentResponse.of(student));
+    }
+
+}
